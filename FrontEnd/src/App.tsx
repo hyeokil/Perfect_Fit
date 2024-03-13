2990605a07192b60de333d1f1234022234d26e7b
정유나 <jyn7077@naver.com>
2024-03-13 17:33:14 +0900

    feat: frontend 초기설정

---
+++ FrontEnd/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>015B</h1>
+      <p>안쏭맞춤 화이팅 하세여!</p>
+      <p>아자아자!!!</p>
+    </>
+  )
+}
+
+export default App
---
+++ FrontEnd/vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister:'auto',
+    }),
+    tsconfigPaths()
+  ],
+});
