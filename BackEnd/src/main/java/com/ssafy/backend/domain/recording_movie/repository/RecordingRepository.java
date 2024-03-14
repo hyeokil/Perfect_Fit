4fc34b3b8325b2b575d80a7f1e43829e8cfb5124
전재완 <wodhksdl7893@naver.com>
2024-03-14 16:37:55 +0900

    feat: 테이블 변경 전 싱글과 듀엣이 한 테이블에 있을 때 코드

--- BackEnd/src/main/java/com/ssafy/backend/domain/recording_movie/repository/RecordingRepository.java
+++ BackEnd/src/main/java/com/ssafy/backend/domain/recording_movie/repository/RecordingRepository.java
@@ -1,6 +1,8 @@
 package com.ssafy.backend.domain.recording_movie.repository;
 
+
 import com.ssafy.backend.domain.recording_movie.entity.Recording;
+import com.ssafy.backend.domain.recording_movie.entity.enums.MultiPlay;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,4 +15,11 @@ public interface RecordingRepository extends JpaRepository<Recording, Long> {
 
     // SingId가 memberId와 일치하는 값을 출력
     List<Recording> findBySingleId(Long singleId);
+
+    // multiId가 null인 Recording 조회
+    List<Recording> findByMultiIsNull();
+
+    List<Recording> findBySingleIdAndDisplayTrue(Long singleId);
+
+    List<Recording> findBySingleIdAndDisplayTrueAndMultiPlayTrue(Long singleId);
 }
