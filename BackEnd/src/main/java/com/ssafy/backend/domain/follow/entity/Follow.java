4a348bd7ae5c2e3d8c7f978203f8f6a5688db02b
김혁일 <rlagurdlf@naver.com>
2024-03-14 14:41:14 +0900

    chore:application.yml 수정

---
+++ BackEnd/src/main/java/com/ssafy/backend/domain/follow/entity/Follow.java
@@ -0,0 +1,16 @@
+package com.ssafy.backend.domain.follow.entity;
+
+import jakarta.persistence.Entity;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class Follow {
+
+
+
+
+}
