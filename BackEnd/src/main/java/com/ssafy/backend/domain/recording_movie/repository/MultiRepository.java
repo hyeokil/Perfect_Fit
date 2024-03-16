028e1f5ad835b337b9af3a174a4da0586651d225
전재완 <wodhksdl7893@naver.com>
2024-03-16 22:39:04 +0900

    feat: multi모드 단일 영상 조회

--- BackEnd/src/main/java/com/ssafy/backend/domain/recording_movie/repository/MultiRepository.java
+++ BackEnd/src/main/java/com/ssafy/backend/domain/recording_movie/repository/MultiRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface MultiRepository extends JpaRepository<Multi, Long> {
@@ -16,4 +17,6 @@ public interface MultiRepository extends JpaRepository<Multi, Long> {
     List<Multi> findByPlayer2IdAndDisplayTrue(Long Player2);
 
     List<Multi> findByDisplayTrueAndPlayer2IdIsNull();
+
+    Optional<Multi> findByIdAndDisplayTrue(Long id);
 }
