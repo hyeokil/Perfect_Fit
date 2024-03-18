926a7b207fbf0429ff5a8e269f76eb8665382cfa
김혁일 <rlagurdlf@naver.com>
2024-03-18 17:59:19 +0900

    refactor: recording domain finish refactor

--- BackEnd/src/main/java/com/ssafy/backend/domain/recording_movie/repository/SingleRepository.java
+++ BackEnd/src/main/java/com/ssafy/backend/domain/recording_movie/repository/SingleRepository.java
@@ -1,5 +1,6 @@
 package com.ssafy.backend.domain.recording_movie.repository;
 
+import com.ssafy.backend.domain.recording_movie.entity.Recording;
 import com.ssafy.backend.domain.recording_movie.entity.Single;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
