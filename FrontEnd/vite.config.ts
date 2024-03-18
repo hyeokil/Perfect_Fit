81ff50ae1004b400740420155bb84623f61d924e
yuna <jyn7077@naver.com>
2024-03-18 23:47:29 +0900

    chore: PWA devOption 수정

--- FrontEnd/vite.config.ts
+++ FrontEnd/vite.config.ts
@@ -10,6 +10,9 @@ export default defineConfig({
     VitePWA({
       registerType: "autoUpdate",
       injectRegister:'auto',
+      devOptions: {
+        enabled: false
+      }
     }),
     tsconfigPaths()
   ],
