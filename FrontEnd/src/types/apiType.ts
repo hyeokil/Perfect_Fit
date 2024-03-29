163e77c982e19d916c639409c27d7534ed66d43d
정유나 <jyn7077@naver.com>
2024-03-29 10:37:06 +0900

    chore: 앨범 커버 주소 수정

---
+++ FrontEnd/src/types/apiType.ts
@@ -0,0 +1,11 @@
+export type MusicInfoType = {
+  songTitle: string,
+  artist: string | null,
+  genre: string,
+  songUrl: string ,
+  songThumbnail: string,
+  songReleaseDate: string,
+  songLength: string ,
+  myListDisplay: boolean,
+  songView: number
+}
