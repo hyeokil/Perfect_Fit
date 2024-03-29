3b18af0bf128cc310ccba037c0d8c3d0fde78298
김재훈 <sirika2547@naver.com>
2024-03-29 20:01:55 +0900

    feat: 조회수 카운트

--- BackEnd/src/main/java/com/ssafy/backend/domain/song/service/SongCountServiceImpl.java
+++ BackEnd/src/main/java/com/ssafy/backend/domain/song/service/SongCountServiceImpl.java
@@ -20,7 +20,7 @@ import java.util.stream.Collectors;
 
 @Slf4j
 @Service
-@Transactional
+//@Transactional
 @RequiredArgsConstructor
 public class SongCountServiceImpl implements SongCountService {
 
@@ -29,18 +29,11 @@ public class SongCountServiceImpl implements SongCountService {
     private final SongHistoryRepository songHistoryRepository;
 
 
-    @Scheduled(cron = "0 20 * * * *")  // 매 시간 정시마다 실행한다. (초 분 시간 일 월 요일(0,7은 일요일, 1~6은 월~토)
+    @Scheduled(cron = "0 0 * * * *")  // 매 시간 정시마다 실행한다. (초 분 시간 일 월 요일(0,7은 일요일, 1~6은 월~토)
     public void updateSongCounts() {
         // 지난 1시간 동안의 song_history 데이터
         List<SongCount> recentCounts = songCountRepository.findCountByHour();
 
-//        // recentCounts에서 count 값을 모두 합산 후 출력
-//        long total = recentCounts.stream()
-//                .mapToLong(SongCount::getCount)
-//                .sum();
-//        System.out.println("Total count in the last hour: " + total);
-
-        // 조회된 결과를 반복하여 처리
         for (SongCount recentCount : recentCounts) {
             // song_count 테이블에서 해당 song_id에 대한 레코드 조회
             SongCount existingCount = songCountRepository.findById(recentCount.getSongId()).orElseThrow();
@@ -48,11 +41,8 @@ public class SongCountServiceImpl implements SongCountService {
             // 새로운 count 값을 기존 값에 가산 (여기서 문제가 되는 부분이다)
             existingCount.setCount(existingCount.getCount() + recentCount.getCount());
 
-
-//            songCountRepository.save(existingCount);
-
-
-
+            // DB에 반영
+            songCountRepository.save(existingCount);
         }
     }
 
