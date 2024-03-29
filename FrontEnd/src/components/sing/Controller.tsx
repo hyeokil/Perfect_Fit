b01001af193c5fb428ddf44ee677ad7a4bda0e2f
정유나 <jyn7077@naver.com>
2024-03-29 09:51:27 +0900

    feat: 피치조절 구현

--- FrontEnd/src/components/sing/Controller.tsx
+++ FrontEnd/src/components/sing/Controller.tsx
@@ -1,103 +1,100 @@
-// import { ChangeEvent, useEffect, useRef, useState } from "react";
-// import { PitchShifter } from "soundtouchjs";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faPause, faPlay } from "@fortawesome/free-solid-svg-icons";
+import { ChangeEvent, useEffect, useRef, useState } from "react";
+import { PitchShifter } from "soundtouchjs";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPause, faPlay } from "@fortawesome/free-solid-svg-icons";
 import styles from "@styles/sing/Controller.module.scss";
 const Controller = () => {
   // -------------------------------------------------------
-  // const [pitch, setPitch] = useState<number>(1.0);
-  // const [tempo, setTempo] = useState<number>(1.0);
-  // const [isPlaying, setIsPlaying] = useState<boolean>(false);
-  // const [shift, setShift] = useState<any>(null);
-  // const audioCtx = new AudioContext();
-  // const state = audioCtx.state;
-  // console.log("현재 피치 : ", pitch);
-  // console.log("오디오 상태 : ", state);
+  const [pitch, setPitch] = useState<number>(1.0);
+  const [tempo, setTempo] = useState<number>(1.0);
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
+  const audioCtx = new AudioContext();
+  const state = audioCtx.state;
+  console.log("현재 피치 : ", pitch);
+  console.log("오디오 상태 : ", state);
 
-  // const shiftRef = useRef<PitchShifter | null>(null);
-  // const audioCtxRef = useRef<AudioContext | null>(null);
+  const shiftRef = useRef<PitchShifter | null>(null);
+  const audioCtxRef = useRef<AudioContext | null>(null);
 
-  // useEffect(() => {
-  //   audioCtxRef.current = new AudioContext();
-  //   return () => {
-  //     // 컴포넌트가 언마운트 될 때 AudioContext를 종료
-  //     audioCtxRef.current?.close();
-  //   };
-  // }, []);
+  useEffect(() => {
+    audioCtxRef.current = new AudioContext();
+    return () => {
+      // 컴포넌트가 언마운트 될 때 AudioContext를 종료
+      audioCtxRef.current?.close();
+    };
+  }, []);
 
-  // const onLoad = async ({ target: { result: buffer } }: any) => {
-  //   if (shiftRef.current) {
-  //     shiftRef.current.disconnect();
-  //   }
-  //   if (buffer) {
-  //     const audioCtx = audioCtxRef.current;
-  //     if (audioCtx) {
-  //       const audioBuffer = await audioCtx.decodeAudioData(buffer);
-  //       const myShift = new PitchShifter(audioCtx, audioBuffer, 16384);
-  //       myShift.tempo = tempo;
-  //       myShift.pitch = pitch;
-  //       setShift(myShift);
-  //       shiftRef.current = myShift;
-  //       if (isPlaying) {
-  //         myShift.connect(audioCtx.destination);
-  //         audioCtx.resume();
-  //       }
-  //     }
-  //   }
-  // };
+  const onLoad = async ({ target: { result: buffer } }: any) => {
+    if (shiftRef.current) {
+      shiftRef.current.disconnect();
+    }
+    if (buffer) {
+      const audioCtx = audioCtxRef.current;
+      if (audioCtx) {
+        const audioBuffer = await audioCtx.decodeAudioData(buffer);
+        const myShift = new PitchShifter(audioCtx, audioBuffer, 16384);
+        myShift.tempo = tempo;
+        myShift.pitch = pitch;
+        shiftRef.current = myShift;
+        if (isPlaying) {
+          myShift.connect(audioCtx.destination);
+          audioCtx.resume();
+        }
+      }
+    }
+  };
 
-  // // 저장되어있는 파일 사용하기
-  // const loadFile = async () => {
-  //   try {
-  //     const res = await fetch("/src/assets/video/[MR] 기리보이 - Zoa.wav");
-  //     const buffer = await res.arrayBuffer();
-  //     onLoad({ target: { result: buffer } });
-  //   } catch (error) {
-  //     console.error;
-  //   }
-  // };
+  // 저장되어있는 파일 사용하기
+  const loadFile = async () => {
+    try {
+      const res = await fetch("/src/assets/sounds/꽃길.mp3");
+      const buffer = await res.arrayBuffer();
+      onLoad({ target: { result: buffer } });
+    } catch (error) {
+      console.error;
+    }
+  };
 
-  // // const newShifter = (buffer) => {
-  // //   const myShift = new PitchShifter(audioCtx, buffer, 16384);
-  // //   setShift(myShift);
-  // //   myShift.tempo = tempo;
-  // //   myShift.pitch = pitch;
-  // //   myShift.connect(audioCtx.destination);
-  // // };
+  useEffect(() => {
+    loadFile();
+    return () => {
+      // 컴포넌트가 언마운트 될 때 PitchShifter를 해제
+      if (shiftRef.current) {
+        shiftRef.current.disconnect();
+      }
+    };
+  }, []);
 
-  // useEffect(() => {
-  //   loadFile();
-  //   return () => {
-  //     // 컴포넌트가 언마운트 될 때 PitchShifter를 해제
-  //     if (shiftRef.current) {
-  //       shiftRef.current.disconnect();
-  //     }
-  //   };
-  // }, []);
+  const togglePlayback = () => {
+    if (shiftRef.current) {
+      if (isPlaying) {
+        shiftRef.current.disconnect();
+      } else {
+        shiftRef.current.connect(audioCtxRef.current!.destination);
+        audioCtxRef.current!.resume();
+      }
+      setIsPlaying(!isPlaying);
+    }
+  };
 
-  // const togglePlayback = () => {
-  //   if (shiftRef.current) {
-  //     if (isPlaying) {
-  //       shiftRef.current.disconnect();
-  //     } else {
-  //       shiftRef.current.connect(audioCtxRef.current!.destination);
-  //       audioCtxRef.current!.resume();
-  //     }
-  //     setIsPlaying(!isPlaying);
-  //   }
-  // };
-
-  // const handleChangePitch = (e: ChangeEvent<HTMLInputElement>) => {
-  //   const value = parseFloat(e.target.value);
-  //   setPitch(value);
-  //   if (shiftRef.current) {
-  //     shiftRef.current.pitch = value;
-  //   }
-  // };
+  const handleChangePitch = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = parseFloat(e.target.value);
+    setPitch(value);
+    if (shiftRef.current) {
+      shiftRef.current.pitch = value;
+    }
+  };
+  const handleChangeTempo = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = parseFloat(e.target.value);
+    setTempo(value);
+    if (shiftRef.current) {
+      shiftRef.current.tempo = value;
+    }
+  };
 
   return (
     <div className={styles.wrapper}>
-      {/* <h1>컨트롤러!!!</h1>
+      <h1>컨트롤러!!!</h1>
       <div className={styles.play}>
         <button className={styles.controlbutton} onClick={togglePlayback}>
           {isPlaying ? (
@@ -120,7 +117,21 @@ const Controller = () => {
           <p>{pitch}</p>
           <label>피치</label>
         </div>
-      </div> */}
+      </div>
+      <div className={styles.controlbox}>
+        <input
+          type="range"
+          onChange={handleChangeTempo}
+          value={tempo}
+          min="0.5"
+          max="2"
+          step="0.01"
+        />
+        <div>
+          <p>{tempo}</p>
+          <label>템포</label>
+        </div>
+      </div>
     </div>
   );
 };
