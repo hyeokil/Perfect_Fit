9d947398e9b041c827b758d954cb0f443032ab30
정유나 <jyn7077@naver.com>
2024-04-01 06:32:39 +0900

    feat: 사용자 음성 학습

---
+++ FrontEnd/src/components/record/Script.tsx
@@ -0,0 +1,77 @@
+import React, { useEffect, useRef } from "react";
+import styles from "@styles/record/script.module.scss";
+
+type ScriptProps = {
+  isTrackingStarted: boolean;
+};
+
+const lyrics = [
+  "동해 물과 백두산이 마르고 닳도록",
+  "하느님이 보우하사 우리나라 만세",
+  "무궁화 삼천리 화려 강산",
+  "대한 사람 대한으로 길이 보전하세"
+];
+
+const Script: React.FC<ScriptProps> = ({ isTrackingStarted }) => {
+  const lyricsRefs = useRef<(HTMLSpanElement | null)[][]>([]);
+
+  useEffect(() => {
+    if (isTrackingStarted) {
+      const animateLyrics = () => {
+        lyricsRefs.current.forEach((lineRefs, lineIndex) => {
+          setTimeout(() => {
+            lineRefs.forEach((charRef, charIndex) => {
+              if (charRef) {
+                charRef.style.color = "black"; // 글자 색상 초기화
+              }
+            });
+          }, 15000 * lineIndex); // 각 라인의 시작 시간 설정
+          
+          setTimeout(() => {
+            lineRefs.forEach((charRef, charIndex) => {
+              if (charRef) {
+                charRef.style.color = " #46B8E0"; // 첫 번째 글자부터 빨간색으로 변경
+              }
+            });
+          }, 15000 * lineIndex + 1000); // 1초 후 첫 번째 글자 색상 변경
+
+          for (let i = 1; i < lineRefs.length; i++) {
+            setTimeout(() => {
+              if (lineRefs[i]) {
+                lineRefs[i].style.color = " #46B8E0"; // 이후 글자들도 빨간색으로 변경
+              }
+            }, 15000 * lineIndex + 1000 + i * 1500); // 1초 후부터 1.5초 간격으로 색상 변경
+          }
+        });
+      };
+
+      animateLyrics();
+    }
+  }, [isTrackingStarted]); // isTrackingStarted가 변경될 때마다 애니메이션 재생
+
+  return (
+    <div className={styles.script}>
+      {lyrics.map((line, lineIndex) => (
+        <div key={lineIndex}>
+          {line.split("").map((char, charIndex) => (
+            <span
+              key={charIndex}
+              ref={(el) => {
+                // lyricsRefs 배열 초기화
+                if (!lyricsRefs.current[lineIndex]) {
+                  lyricsRefs.current[lineIndex] = [];
+                }
+                lyricsRefs.current[lineIndex][charIndex] = el;
+              }}
+              className={styles.lyricsline}
+            >
+              {char}
+            </span>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Script;
