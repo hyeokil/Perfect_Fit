fe72b5e5a58ad0415a9c863d72bbba6bab8a7a86
정유나 <jyn7077@naver.com>
2024-04-01 09:43:22 +0900

    chore: 충돌해결

--- FrontEnd/src/store/useRecordStore.ts
+++ FrontEnd/src/store/useRecordStore.ts
@@ -1,6 +1,5 @@
 import { create } from "zustand";
-import {persist} from 'zustand/middleware'
-
+import { persist } from "zustand/middleware";
 
 type RecordType = {
   isPlaying: boolean;
@@ -23,26 +22,34 @@ type RecordType = {
   setDisplayUrl: (displayUrl: string | null) => void;
 };
 
-const useRecordStore = create<RecordType>((set) => ({
-  // true => reset false => reset x
-  resetRecord: true,
-  isPlaying: false,
-  isRecording: false,
-  videoBlob: [],
-  displayBlob: [],
-  musicBlob: null,
-  videoUrl: null,
-  musicUrl: null,
-  displayUrl: null,
-  setIsPlaying: (isPlaying: boolean) => set({ isPlaying }),
-  setMusicUrl: (musicUrl: string | null) => set({ musicUrl }),
-  setVideoUrl: (videoUrl: string | null) => set({ videoUrl }),
-  setDisplayUrl: (displayUrl: string | null) => set({ displayUrl }),
-  setIsRecording: (isRecording: boolean) => set({ isRecording }),
-  setResetRecord: (resetRecord: boolean) => set({ resetRecord }),
-  setVideoBlob: (videoBlob: Blob[]) => set({ videoBlob }),
-  setDisplayBlob: (displayBlob: Blob[]) => set((state) => ({ ...state, displayBlob })),
-  setMusicBlob: (musicBlob: Blob | null) => set({ musicBlob }),
-}));
+const useRecordStore = create(
+  persist<RecordType>(
+    (set) => ({
+      // true => reset false => reset x
+      resetRecord: true,
+      isPlaying: false,
+      isRecording: false,
+      videoBlob: [],
+      displayBlob: [],
+      musicBlob: null,
+      videoUrl: null,
+      musicUrl: null,
+      displayUrl: null,
+      setIsPlaying: (isPlaying: boolean) => set({ isPlaying }),
+      setMusicUrl: (musicUrl: string | null) => set({ musicUrl }),
+      setVideoUrl: (videoUrl: string | null) => set({ videoUrl }),
+      setDisplayUrl: (displayUrl: string | null) => set({ displayUrl }),
+      setIsRecording: (isRecording: boolean) => set({ isRecording }),
+      setResetRecord: (resetRecord: boolean) => set({ resetRecord }),
+      setVideoBlob: (videoBlob: Blob[]) => set({ videoBlob }),
+      setDisplayBlob: (displayBlob: Blob[]) =>
+        set((state) => ({ ...state, displayBlob })),
+      setMusicBlob: (musicBlob: Blob | null) => set({ musicBlob }),
+    }),
+    {
+      name: "recordStorage",
+    }
+  )
+);
 
 export default useRecordStore;
