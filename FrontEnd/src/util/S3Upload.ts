253f65e553958cc34e160243858985401c8012c9
정유나 <jyn7077@naver.com>
2024-04-02 06:33:43 +0900

    feat: 영상 저장 S3 수정

--- FrontEnd/src/util/S3Upload.ts
+++ FrontEnd/src/util/S3Upload.ts
@@ -1,24 +1,13 @@
-// import AWS from "aws-sdk";
 import AWS from "aws-sdk";
 
-// const blobToFile = (blob) => {
-//   // Blob에서 File을 생성합니다.
-//   const file = new File([blob], 'music.mp3', { type: 'audio/mp3' }); // 파일 이름과 MIME 타입을 지정합니다.
-//   return file;
-// };
-//------------------------------------------------------
-const S3Upload = (blob: Blob) => {
-  // Blob을 File로 변환합니다.
-  // const file = blobToFile(blob);
-
-  //----------기본 정보----------------------------------
-  console.log("env, ", import.meta.env.VITE_REGION);
-  console.log("env, ", import.meta.env.VITE_ACCESS_KEY_ID);
-  console.log("env, ", import.meta.env.VITE_SECRET_ACCESS_KEY_ID);
+const S3Upload = (blob: Blob, fileCase: string): Promise<string> => {
+  // 환경 변수 설정
   const REGION = import.meta.env.VITE_REGION;
   const ACCESS_KEY_ID = import.meta.env.VITE_ACCESS_KEY_ID;
   const SECRET_ACCESS_KEY_ID = import.meta.env.VITE_SECRET_ACCESS_KEY_ID;
   const BUCKET = import.meta.env.VITE_BUCKET;
+
+  // AWS 설정
   AWS.config.update({
     region: REGION,
     credentials: new AWS.Credentials(ACCESS_KEY_ID, SECRET_ACCESS_KEY_ID),
@@ -29,25 +18,23 @@ const S3Upload = (blob: Blob) => {
     region: REGION,
   });
 
-  //-----------------------------------------------------
-
+  // 업로드 매개변수 설정
   const uploadParams = {
     ACL: "private",
     Bucket: BUCKET,
-    Key: `helpme`, // 업로드될 파일의 키 설정
-    Body: blob, // 변환된 File 객체를 사용합니다.
+    Key: fileCase,
+    Body: blob,
   };
 
-  const upload = s3.upload(uploadParams);
-
-  upload
-    .promise()
+  // S3 업로드 프로미스 반환
+  return s3.upload(uploadParams).promise()
     .then((res) => {
-      console.log(res);
       console.log("업로드 성공");
+      return res.Location; // 업로드 성공 시 Location 반환
     })
     .catch((err) => {
       console.error("업로드 실패:", err);
+      throw err; // 에러 발생 시 예외 던지기
     });
 };
 
